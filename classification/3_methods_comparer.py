83ab5974233bd6547e1a27475aab28329007b17c
Mathiaz21 <mathias.gilbert42@gmail.com>
2025-04-07 18:42:27 +0200

    Little work session

--- classification/3_methods_comparer.py
+++ classification/3_methods_comparer.py
@@ -1,8 +1,10 @@
 import numpy as np
 import utils.serve_data as sd
+import models.logistic_regression as lr
 
 from sklearn import model_selection
 from utils.basic_operations import standardize_data
+
 
 # Parameters
 K_outer: int = 10
@@ -11,29 +13,44 @@ LAMBDA_START: float = .2
 LAMBDA_STOP: float = 10.
 LAMBDA_STEP: float = .2
 
-# Models
-
-def get_baseline_error(y_train, y_test):
-  largest_class: str = "B" if np.sum(y_train == "B") >= np.sum(y_test == "M") else "M"
-  error: float = np.sum(y_test != largest_class) / len(y_test)
-  return error
-
-
 # Setup
 X = sd.get_features_dataset()
 y = sd.get_targets()
 X = standardize_data(X)
 N, M = X.shape
 
-lambdas: list[float] = np.arange(stop=LAMBDA_STOP, start=LAMBDA_START, step=LAMBDA_STEP)
-nb_lam: int = len(lambdas)
+LAMBDAS: list[float] = np.arange(stop=LAMBDA_STOP, start=LAMBDA_START, step=LAMBDA_STEP)
+nb_lam: int = len(LAMBDAS)
 outer_fold = model_selection.KFold(n_splits=K_outer, shuffle=True)
 
+
+# Models
+def get_baseline_error(y_train, y_test) -> float:
+  largest_class: str = "B" if np.sum(y_train == "B") >= np.sum(y_test == "M") else "M"
+  error: float = np.sum(y_test != largest_class) / len(y_test)
+  return error
+
+
+def get_regression_error(m, X_test, y_test) -> float: 
+  y_est = m.predict(X_test)
+  return np.sum(y_est != y_test) / len(y_test)
+
+def compute_regression_errors(cursor: int, error_df: np.ndarray, X_train, y_train, X_test, y_test) -> None:
+  for id, lam in enumerate(LAMBDAS):
+    m = lr.generate_logistic_regression(regularization_parameter=lam).fit(X_train, y_train)
+    error_df[cursor, id] = get_regression_error(m, X_test, y_test)
+
+cursor_outer: int = 0
+baseline_outer_error = np.empty((K_outer, 1))
+regression_outer_error = np.empty((K_outer, 1))
+regression_lambdas= np.empty((K_outer, 1))
 for outer_train_index, outer_test_index in outer_fold.split(X):
 
   X_inner = X[outer_train_index]
   inner_fold = model_selection.KFold(n_splits=K_inner, shuffle=True)
-  min_baseline_error = None
+  cursor_inner: int = 0
+  regression_inner_error = np.empty((K_inner, nb_lam))
+  baseline_inner_error = np.empty((K_inner, 1))
   for inner_train_index, inner_test_index in inner_fold.split(X_inner):
 
     X_train = X[inner_train_index,:]
@@ -41,9 +58,15 @@ for outer_train_index, outer_test_index in outer_fold.split(X):
     X_test = X[inner_test_index,:]
     y_test = y[inner_test_index]
 
-    baseline_error: float = get_baseline_error(y_train=y_train, y_test=y_test)
-    min_baseline_error = baseline_error if min_baseline_error is None or min_baseline_error > baseline_error else min_baseline_error
+    baseline_inner_error[cursor_inner] = get_baseline_error(y_train=y_train, y_test=y_test)
+    compute_regression_errors(cursor=cursor_inner, error_df=regression_inner_error, X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test)
 
-  print(f"Baseline error: {min_baseline_error}")
+    cursor_inner += 1
+  
+  baseline_outer_error[cursor_outer] = np.min(baseline_inner_error)
+  regression_outer_error[cursor_outer] = np.min( np.mean()) # get the mean error for each inner fold, get the best lambda, save that
+
+  cursor_outer += 1
+
 
 
