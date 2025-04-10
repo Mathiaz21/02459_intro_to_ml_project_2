c0944f9f91cd74c1af8dbb91db19264cf8062930
Mathiaz21 <mathias.gilbert42@gmail.com>
2025-04-10 13:57:22 +0200

    Adding feature selection

--- utils/serve_data.py
+++ utils/serve_data.py
@@ -8,15 +8,19 @@ from pandas import read_csv
 X_DATA_PATH: str = "local_data/breast_cancer_wisconsin_features.csv"
 Y_DATA_PATH: str = "local_data/breast_cancer_wisconsin_targets.csv"
 
-def verify_data_existence() -> None:
+def _verify_data_existence() -> None:
   if not (os.path.exists(X_DATA_PATH) and os.path.exists(Y_DATA_PATH)):
     print("Data not saved locally, importing it")
     srd.save_data()
 
 def get_features_dataset() -> np.ndarray:
-  verify_data_existence()
+  _verify_data_existence()
   return read_csv(X_DATA_PATH).to_numpy()
 
 def get_targets() -> np.ndarray:
-  verify_data_existence()
+  _verify_data_existence()
   return np.ravel(read_csv(Y_DATA_PATH))
+
+def get_attribute_names() -> list[str]:
+  _verify_data_existence()
+  return read_csv(X_DATA_PATH).columns
